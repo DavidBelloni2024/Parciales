294a0650e90b9e8586dd7ce9987c09d31d2ed288
DavidBelloni2024 <david.djb@live.com.ar>
2024-06-02 18:50:04 -0300

    Add files via upload

---
+++ SL/DAL/Tools/SqlHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace SL.DAL.Tools
+{
+    public static class SqlHelper
+    {
+        readonly static string ConString;
+        static SqlHelper()
+        {
+            ConString = ConfigurationManager.ConnectionStrings["MainConString"].ConnectionString;
+        }
+
+        public static Int32 ExecuteNonQuery(String commandText,CommandType commandType, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(ConString))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    // There're three command types: StoredProcedure, Text, TableDirect. The TableDirect 
+                    // type is only for OLE DB.
+
+                    cmd.CommandType = commandType;
+                    cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static Object ExecuteScalar(String commandText,CommandType commandType, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(ConString))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    cmd.CommandType = commandType;
+                    cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public static SqlDataReader ExecuteReader(String commandText,CommandType commandType, params SqlParameter[] parameters)
+        {
+            SqlConnection conn = new SqlConnection(ConString);
+            string s = commandText;
+            using (SqlCommand cmd = new SqlCommand(commandText, conn))
+            {
+                cmd.CommandType = commandType;
+                cmd.Parameters.AddRange(parameters);
+
+                conn.Open();
+                // When using CommandBehavior.CloseConnection, the connection will be closed when the 
+                // IDataReader is closed.
+                SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                return reader;
+            }
+        }
+
+
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,60 @@
+﻿using BLL;
+using BLL.Services;
+using DOMAIN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Establecer el costo de embarque inicial
+            //Boleto.CostoEmbarque = 100;
+
+            Boleto boleto1 = new Turista(1, DateTime.Now, 7, 100);
+            BoletoService.Current.Add(boleto1);
+
+            /*
+            Venta venta = new Venta();
+
+            //Despliegue de Informacion Comercial
+            Console.WriteLine($" INFORMACIÓN COMERCIAL ");
+            Console.WriteLine($"¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ \n");
+            Console.WriteLine($"▬ TARIFA BOLETO TURISTA: $18350" );
+            Console.WriteLine($"▬ TARIFA BOLETO EJECUTIVO: $19750");
+            Console.WriteLine($"▬ COSTO DE EMBARQUE: $100 \n");
+
+            // Crear boletos
+            Boleto boletoTurista1 = new Turista(1, DateTime.Now, 7);
+            Boleto boletoTurista2 = new Turista(2, DateTime.Now, 5);
+            Boleto boletoEjecutivo1 = new Ejecutivo(3, DateTime.Now, 7);
+            Boleto boletoEjecutivo2 = new Ejecutivo(4, DateTime.Now, 8);
+            Boleto boletoEjecutivo3 = new Ejecutivo(5, DateTime.Now, 10);
+
+            // Agregar boletos a la venta
+            venta.AgregarBoleto(boletoTurista1);
+            venta.AgregarBoleto(boletoTurista2);
+            venta.AgregarBoleto(boletoEjecutivo1);
+            venta.AgregarBoleto(boletoEjecutivo2);
+            venta.AgregarBoleto(boletoEjecutivo3);
+
+
+            // Listar todos los boletos vendidos
+            Console.WriteLine($" VOLETOS VENDIDOS ");
+            Console.WriteLine($"¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ \n");
+
+            foreach (var boleto in venta.ListarTodos())
+            {
+                Console.WriteLine($" ♦ Boleto Numero: {boleto.Numero}, Tipo: {boleto.GetType().Name}, Costo: {boleto.CostoBoleto()}, Fecha Regreso: {boleto.CalcularRegreso()}");
+            }
+            */
+            Console.ReadKey();
+        }
+    }
+}
