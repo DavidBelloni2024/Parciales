b27696f920abf16b715a69c2c3b051c9754830e7
DavidBelloni2024 <david.djb@live.com.ar>
2024-06-02 18:49:30 -0300

    Add files via upload

---
+++ Enums/ColumnasBoleto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Enums
+{
+    public enum ColumnasBoleto
+    {
+        [Description("Numero")] Numero,
+        [Description("FechaSalida")] FechaSalida,
+        [Description("TiempoEnDias")] TiempoEnDias,
+        [Description("CostoEmbarque")] CostoEmbarque, 
+    }
+}
